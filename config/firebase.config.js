e79c642de8da520ec4017220f37bae567a17ec4e
klein-l <kleinwisdomm@gmail.com>
2023-09-05 10:49:39 -0400

    firestore

---
+++ config/firebase.config.js
@@ -0,0 +1,12 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8_2XqJLnN66KGza8lAJR8kHsNa6ABg9A",
+  authDomain: "v-chat-application-9246f.firebaseapp.com",
+  projectId: "v-chat-application-9246f",
+  storageBucket: "v-chat-application-9246f.appspot.com",
+  messagingSenderId: "999551678471",
+  appId: "1:999551678471:web:be26ca3fd9add0781dd2a2",
+};
